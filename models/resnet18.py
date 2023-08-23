e441019faacf791c3caf99d586dd1570c1a06dcf
xh <33172894+xuexinghua@users.noreply.github.com>
2023-08-23 22:10:37 +0800

    Update resnet18.py

--- models/resnet18.py
+++ models/resnet18.py
@@ -8,8 +8,6 @@ from layer.winograd_layers import winconv2d_fi, winconv2d
 from layer.fft_layer import FFTConv2d_fi, FFTConv2d
 tiles = 2
 
-
-
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -25,23 +23,16 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         residual = x
-
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-
         out = self.conv2(out)
         out = self.bn2(out)
-
         if self.downsample is not None:
             residual = self.downsample(x)
-
         out += residual
         out = self.relu(out)
-
         return out
-
-
 
 class fi_BasicBlock(nn.Module):
     expansion = 1
@@ -62,13 +53,10 @@ class fi_BasicBlock(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-
         out = self.conv2(out)
         out = self.bn2(out)
-
         if self.downsample is not None:
             residual = self.downsample(x)
-
         out += residual
         out = self.relu(out)
 
@@ -90,24 +78,17 @@ class fi_win_BasicBlock(nn.Module):
 
     def forward(self, x):
         residual = x
-
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-
         out = self.conv2(out)
         out = self.bn2(out)
-
         if self.downsample is not None:
             residual = self.downsample(x)
-
         out += residual
         out = self.relu(out)
 
         return out
-
-
-
 
 class ResNet(nn.Module):
 
@@ -115,8 +96,7 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
 
         self.ber = ber
-        self.bit = bit   
-        
+        self.bit = bit           
         self.inplanes = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
@@ -159,19 +139,15 @@ class ResNet(nn.Module):
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
-
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
         return x
-
-
 
 def fi_resnet18(ber, bit, pretrained=False, **kwargs):
 
@@ -185,10 +161,3 @@ def fi_win_resnet18(ber, bit, pretrained=False, **kwargs):
     model = ResNet(ber, bit, fi_win_BasicBlock, BasicBlock, [2, 2, 2, 2], **kwargs)
 
     return model
-
-
-
-
-
-
-
