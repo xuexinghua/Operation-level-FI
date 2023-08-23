6db8969593f8ce08adcd4ab66ca11397cae2babf
xh <33172894+xuexinghua@users.noreply.github.com>
2023-08-23 20:30:06 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -6,8 +6,8 @@ import torchvision.datasets as datasets
 
 
 def data_loader(root, batch_size=256, workers=1, pin_memory=True):
-    traindir = os.path.join(root, 'ILSVRC2012_img_train')
-    valdir = os.path.join(root, 'ILSVRC2012_img_val')
+    traindir = os.path.join(root, 'train')
+    valdir = os.path.join(root, 'val')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
@@ -46,3 +46,34 @@ def data_loader(root, batch_size=256, workers=1, pin_memory=True):
         pin_memory=pin_memory
     )
     return train_loader, val_loader
+    
+def test_loader(root, batch_size=256, workers=1, pin_memory=True):
+    
+    testdir = os.path.join(root, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+
+    test_dataset = datasets.ImageFolder(
+        testdir,
+        transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize
+        ])
+    )
+
+    a = len(test_dataset)- 2000
+    
+    test_dataset, other = torch.utils.data.random_split(test_dataset,[2000,a]) 
+    
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=workers,
+        pin_memory=pin_memory
+    )
+    return test_loader
+    
