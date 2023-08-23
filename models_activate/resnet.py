7d181634917f8e9128cac4606377f719bfa5d10e
xh <33172894+xuexinghua@users.noreply.github.com>
2023-08-23 20:54:49 +0800

    Add files via upload

---
+++ models_activate/resnet.py
@@ -0,0 +1,416 @@
+import os
+import math
+import torch
+import torch.nn as nn
+import torchvision.models
+from layer.activate_layer import BatchNorm2d_fi, ReLU_fi, MaxPool2d_fi, AvgPool2d_fi
+  
+#batchnorm
+class BatchNorm2d_fi_Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, ber, bit, inplanes, planes, stride=1, downsample=None):
+        super(BatchNorm2d_fi_Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = BatchNorm2d_fi(planes, ber, bit)
+        self.conv2 = nn.Conv2d(planes, planes, 3, stride, padding=1, bias=False)
+        
+        self.bn2 = BatchNorm2d_fi(planes, ber, bit)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = BatchNorm2d_fi(planes * 4, ber, bit)
+        self.relu = nn.ReLU()        
+        
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual       
+        out = self.relu(out)
+
+        return out   
+
+
+
+class BatchNorm2d_fi_ResNet(nn.Module):
+
+    def __init__(self, ber, bit, block, layers, num_classes=1000):
+        super(BatchNorm2d_fi_ResNet, self).__init__()
+
+        self.ber = ber
+        self.bit = bit    
+            
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = BatchNorm2d_fi(64, ber, bit)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1,
+                          stride=stride, bias=False),
+                BatchNorm2d_fi(planes * block.expansion, self.ber, self.bit),
+            )
+
+        layers = []
+        layers.append(block(self.ber, self.bit, self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.ber, self.bit, self.inplanes, planes, 1))
+
+        return nn.Sequential(*layers)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+        
+def BatchNorm2d_fi_resnet50(ber, bit, pretrained=False, **kwargs):
+ 
+    model = BatchNorm2d_fi_ResNet(ber, bit, BatchNorm2d_fi_Bottleneck, [3, 4, 6, 3], **kwargs)
+    return model
+
+
+        
+# relu
+class ReLU_fi_Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, ber, bit, inplanes, planes, stride=1, downsample=None):
+        super(ReLU_fi_Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, 3, stride, padding=1, bias=False)
+        
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = ReLU_fi(ber, bit)        
+        
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual       
+        out = self.relu(out)
+
+        return out   
+
+
+
+class ReLU_fi_ResNet(nn.Module):
+
+    def __init__(self, ber, bit, block, layers, num_classes=1000):
+        super(ReLU_fi_ResNet, self).__init__()
+
+        self.ber = ber
+        self.bit = bit    
+            
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = ReLU_fi(ber, bit)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1,
+                          stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.ber, self.bit, self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.ber, self.bit, self.inplanes, planes, 1))
+
+        return nn.Sequential(*layers)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+        
+def ReLU_fi_resnet50(ber, bit, pretrained=False, **kwargs):
+ 
+    model = ReLU_fi_ResNet(ber, bit, ReLU_fi_Bottleneck, [3, 4, 6, 3], **kwargs)
+
+    return model
+
+
+        
+# maxpool
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, ber, bit, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, 3, stride, padding=1, bias=False)
+        
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU()       
+        
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual       
+        out = self.relu(out)
+
+        return out   
+
+
+class MaxPool2d_fi_ResNet(nn.Module):
+
+    def __init__(self, ber, bit, block, layers, num_classes=1000):
+        super(MaxPool2d_fi_ResNet, self).__init__()
+
+        self.ber = ber
+        self.bit = bit    
+            
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = MaxPool2d_fi(ber, bit, kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1,
+                          stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.ber, self.bit, self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.ber, self.bit, self.inplanes, planes, 1))
+
+        return nn.Sequential(*layers)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+        
+def MaxPool2d_fi_resnet50(ber, bit, pretrained=False, **kwargs):
+ 
+    model = MaxPool2d_fi_ResNet(ber, bit, Bottleneck, [3, 4, 6, 3], **kwargs)
+
+    return model
+              
+
+
+
+# avgpool
+
+
+class AvgPool2d_fi_ResNet(nn.Module):
+
+    def __init__(self, ber, bit, block, layers, num_classes=1000):
+        super(AvgPool2d_fi_ResNet, self).__init__()
+
+        self.ber = ber
+        self.bit = bit    
+            
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = AvgPool2d_fi(ber, bit, 7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1,
+                          stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.ber, self.bit, self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.ber, self.bit, self.inplanes, planes, 1))
+
+        return nn.Sequential(*layers)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+        
+def AvgPool2d_fi_resnet50(ber, bit, pretrained=False, **kwargs):
+ 
+    model = AvgPool2d_fi_ResNet(ber, bit, Bottleneck, [3, 4, 6, 3], **kwargs)
+
+    return model
+
+
+
+
+              
--- models_activate/vgg.py
+++ models_activate/vgg.py
@@ -1,1 +1,145 @@
 
+import torch
+import torch.nn as nn
+from layer.activate_layer import BatchNorm2d_fi, ReLU_fi, MaxPool2d_fi, AvgPool2d_fi
+
+
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+
+class BatchNorm2d_fi_VGG(nn.Module):
+    def __init__(self, vgg_name, ber, bit):
+        super(BatchNorm2d_fi_VGG, self).__init__()
+        
+        self.ber = ber
+        self.bit = bit
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 100)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+
+            else:
+                layers += [nn.Conv2d(in_channels, x, 3, stride=1, padding=1, bias=True),
+                           BatchNorm2d_fi(x, self.ber, self.bit),
+                           nn.ReLU()]    
+                in_channels = x     
+                           
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
+
+class ReLU_fi_VGG(nn.Module):
+    def __init__(self, vgg_name, ber, bit):
+        super(ReLU_fi_VGG, self).__init__()
+        
+        self.ber = ber
+        self.bit = bit
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 100)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+
+            else:
+                layers += [nn.Conv2d(in_channels, x, 3, stride=1, padding=1, bias=True),
+                           nn.BatchNorm2d(x),
+                           ReLU_fi(self.ber, self.bit)]    
+                in_channels = x     
+                           
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+        
+        
+        
+class MaxPool2d_fi_VGG(nn.Module):
+    def __init__(self, vgg_name, ber, bit):
+        super(MaxPool2d_fi_VGG, self).__init__()
+        
+        self.ber = ber
+        self.bit = bit
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 100)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [MaxPool2d_fi(self.ber, self.bit, kernel_size=2, stride=2)]
+
+            else:
+                layers += [nn.Conv2d(in_channels, x, 3, stride=1, padding=1, bias=True),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU()]    
+                in_channels = x     
+                           
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+        
+        
+class AvgPool2d_fi_VGG(nn.Module):
+    def __init__(self, vgg_name, ber, bit):
+        super(AvgPool2d_fi_VGG, self).__init__()
+        
+        self.ber = ber
+        self.bit = bit
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 100)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+
+            else:
+                layers += [nn.Conv2d(in_channels, x, 3, stride=1, padding=1, bias=True),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU()]    
+                in_channels = x     
+                           
+        layers += [AvgPool2d_fi(self.ber, self.bit, kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)        
+                
+        
